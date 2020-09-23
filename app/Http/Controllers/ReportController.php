c65ff6d9e63ba2a0622a9ad8960709600e6d5bcd
Arya Bayu <aryabayu23@gmail.com>
2020-09-23 17:52:58 +0700

    update report witel total footer

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -24,7 +24,8 @@ class ReportController extends Controller
         SUM(CASE WHEN Y.STATUS_OPLANG = 1 THEN 1 ELSE 0 END) SUKSES,
         SUM(CASE WHEN Y.STATUS_OPLANG = 2 THEN 1 ELSE 0 END) ANOMALI,
         SUM(CASE WHEN Y.STATUS_OPLANG = 3 THEN 1 ELSE 0 END) GAGAL,
-        SUM(CASE WHEN Y.STATUS_OPLANG IS NULL THEN 1 ELSE 0 END) BELUM_VERIF
+        SUM(CASE WHEN Y.STATUS_OPLANG = 0 THEN 1 ELSE 0 END) BELUM_INPUT,
+        COUNT(*) DAPROS
         FROM(
             SELECT A.*, B.PENAWARAN, C.SPEED_S UP_SPEED, B.PRICE + A.HARGA_ADDON - A.ABONEMEN HARGA, SUBSTRING(A.PERIODE_TAG,5,6) BULAN_TAGIHAN FROM(
             SELECT A.*,B.*,C.ADDON, CASE WHEN C.PRICE IS NOT NULL THEN C.PRICE ELSE 0 END HARGA_ADDON,D.AREA 
@@ -43,13 +44,20 @@ class ReportController extends Controller
 
         foreach ($query as $i => $v) {
 
+            $agree = $v->belum_input + $v->sukses + $v->anomali + $v->gagal;
+            $dec_dapros = floatval($v->dapros);
+            $ach = number_format($agree / $dec_dapros * 100,2);
+
             $response['data'][] = [
                 ++$i,
-                $v->area,
-                $v->sukses,
-                $v->anomali,
-                $v->gagal,
-                $v->belum_verif
+                $v->area, 
+                number_format($v->dapros),
+                number_format($agree),
+                number_format($v->belum_input),
+                number_format($v->sukses),
+                number_format($v->anomali),
+                number_format($v->gagal),
+                $ach.'%'
             ];
         }
 
--- resources/views/report_witel.blade.php
+++ resources/views/report_witel.blade.php
@@ -1,3 +1,8 @@
+<style>
+    .right{
+        text-align: right;
+    }
+</style>
 <h1 class="h3 mb-2 text-gray-800">REPORT WITEL</h1>
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
@@ -28,14 +33,29 @@
                 <tr>
                     <th>#</th>
                     <th>WITEL</th>
+                    <th>DAPROS</th>
+                    <th>AGREE</th>
+                    <th>BELUM INPUT</th>
                     <th>SUKSES</th>
                     <th>ANOMALI</th>
                     <th>GAGAL</th>
-                    <th>BELUM DI INPUT</th>
+                    <th>ACH</th>
                 </tr>
               </thead>
               <tbody>
               </tbody>
+              <tfoot>
+                  <tr>
+                    <th colspan="2" style="text-align:right">Total:</th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                    <th class="right"></th>
+                </tr>
+              </tfoot>
             </table>
         </div>
     </div>
@@ -48,6 +68,76 @@
 
     function loadData(){
         $('#dataTable').DataTable({
+            footerCallback: function ( row, data, start, end, display ) {
+                var api = this.api(), data;
+    
+                var intVal = function ( i ) {
+                    return typeof i === 'string' ?
+                        i.replace(/[\$,]/g, '')*1 :
+                        typeof i === 'number' ?
+                            i : 0;
+                };
+    
+                var dapros = api
+                    .column( 2 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+                    
+                var agree = api
+                    .column( 3 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+                    
+                var belum_input = api
+                    .column( 4 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+                    
+                var sukses = api
+                    .column( 5 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+                    
+                var anomali = api
+                    .column( 6 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+
+                var gagal = api
+                    .column( 7 )
+                    .data()
+                    .reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                    }, 0 );
+
+                var ach_d = api
+                    .column(8)
+                    .data();
+
+                var sum_ach = 0;
+                $.each(ach_d,function(i,val){
+                    sum_ach += parseFloat(val);
+                });
+                var sum_total_ach = sum_ach.toFixed(2);
+
+                $( api.column( 2 ).footer() ).html(numberFormat(dapros));
+                $( api.column( 3 ).footer() ).html(numberFormat(agree));
+                $( api.column( 4 ).footer() ).html(numberFormat(belum_input));
+                $( api.column( 5 ).footer() ).html(numberFormat(sukses));
+                $( api.column( 6 ).footer() ).html(numberFormat(anomali));
+                $( api.column( 7 ).footer() ).html(numberFormat(gagal));
+                $( api.column( 8 ).footer() ).html(sum_total_ach+'%');
+            },
             asynchronous: true,
             processing: true, 
             destroy: true,
@@ -60,15 +150,30 @@
             },
             columns: [
                 { name: 'id', searchable: false, orderable: true, className: 'text-center' },
-                { name: 'witel' },
-                { name: 'sukses' },
-                { name: 'anomali' },
-                { name: 'gagal' },
-                { name: 'belum_verif' },
+                { name: 'witel'},
+                { name: 'dapros', className: 'right'},
+                { name: 'agree', className: 'right'},
+                { name: 'belum_input', className: 'right'},
+                { name: 'sukses', className: 'right'},
+                { name: 'anomali', className: 'right'},
+                { name: 'gagal', className: 'right'},
+                { name: 'ach',className : 'right'}
             ],
             order: [[0, 'asc']],
             iDisplayInLength: 20,
-            pageLength: 50
+            pageLength: 50,
         });
     }
+
+    function numberFormat(nStr) {
+            nStr += '';
+            x = nStr.split(',');
+            x1 = x[0];
+            x2 = x.length > 1 ? '.' + x[1] : '';
+            var rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1)) {
+              x1 = x1.replace(rgx, '$1' + ',' + '$2');
+            }
+            return x1 + x2;
+        }
 </script>
